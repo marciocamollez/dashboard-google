554b49d49f6fdf0fb59f4d74baea25896986171b
Marcio <marciocamollez@hotmail.com>
2021-05-14 14:58:56 -0300

    first commit

--- src/dashboard.test.js
+++ src/dashboard.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Dashboard from './dashboard';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Dashboard />);
+  const linkElement = screen.getByText(/Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
